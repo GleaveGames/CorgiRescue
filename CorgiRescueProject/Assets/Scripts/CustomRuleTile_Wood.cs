7a1bee98e2c8668b0bd23aee34a93ba3a447525e
ilovecodinglol <62714727+ilovecodinglol@users.noreply.github.com>
2021-10-14 15:04:36 +0100

    added to custom rule tile so that wood and stone connect. Needs lots of work though

--- CorgiRescueProject/Assets/Scripts/CustomRuleTile_Wood.cs
+++ CorgiRescueProject/Assets/Scripts/CustomRuleTile_Wood.cs
@@ -7,18 +7,42 @@ using UnityEngine.Tilemaps;
 public class CustomRuleTile_Wood: RuleTile<CustomRuleTile_Wood.Neighbor> {
 
     public bool customField;
+    public SibingGroup siblingGroup;
+
+    public enum SibingGroup
+    {
+        Wood,
+        Rock,
+        Dirt
+    }
+
 
     public class Neighbor : RuleTile.TilingRule.Neighbor {
         public const int Null = 3;
         public const int NotNull = 4;
     }
 
-    public override bool RuleMatch(int neighbor, TileBase tile) {
-        switch (neighbor) {
-            case Neighbor.Null: return tile == null;
-            case Neighbor.NotNull: return tile != null;
+    public override bool RuleMatch(int neighbor, TileBase other) {
+        
+
+        if (other is RuleOverrideTile)
+            other = (other as RuleOverrideTile).m_InstanceTile;
+
+        switch (neighbor)
+        {
+            case TilingRule.Neighbor.This:
+                {
+                    return other is CustomRuleTile_Wood
+                        && (other as CustomRuleTile_Wood).siblingGroup == this.siblingGroup;
+                }
+            case TilingRule.Neighbor.NotThis:
+                {
+                    return !(other is CustomRuleTile_Wood
+                        && (other as CustomRuleTile_Wood).siblingGroup == this.siblingGroup);
+                }
         }
-        return base.RuleMatch(neighbor, tile);
+
+        return base.RuleMatch(neighbor, other);
     }
 
     public override void RefreshTile(Vector3Int position, ITilemap tilemap)
@@ -36,4 +60,5 @@ public class CustomRuleTile_Wood: RuleTile<CustomRuleTile_Wood.Neighbor> {
             //base.RefreshTile(position, tilemap);
         }
     }
+
 }
