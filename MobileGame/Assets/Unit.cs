4480a130a21c24b4ea90bfdbcaaa197e931396e9
ilovecodinglol <62714727+ilovecodinglol@users.noreply.github.com>
2021-11-30 21:30:07 +0000

    Added the unit object and a little canvas for it! Need to add Cases for the update

---
+++ MobileGame/Assets/Unit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Unit : MonoBehaviour
+{
+    public int health;
+    public int attack;
+    Text healthText;
+    Text attackText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText = transform.GetChild(0).GetChild(0).GetComponent<Text>();
+        attackText = transform.GetChild(0).GetChild(1).GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = health.ToString();
+        attackText.text = attack.ToString();
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
+}
