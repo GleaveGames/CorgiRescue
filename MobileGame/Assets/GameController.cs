168cb7c9f8d4f324b7354276617d72ba43adfb5e
ilovecodinglol <62714727+ilovecodinglol@users.noreply.github.com>
2021-11-30 14:32:04 +0000

    SomeLittleThings and new mobile game

---
+++ MobileGame/Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject draggingObj;
+
+
+    private void Update()
+    {
+        //stop dragging if mouse up
+        if (Input.GetMouseButtonUp(0)) draggingObj = null;
+
+
+        if(draggingObj != null)
+        {
+            draggingObj.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ MobileGame/Assets/ShopSprite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopSprite : MonoBehaviour
+{
+    float dist;
+    bool dragging = false;
+    Vector3 offset;
+    Transform toDrag;
+    GameController gc;
+
+    private void Start()
+    {
+        gc = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+
+            if (targetObject != null) Debug.Log(targetObject.name);
+
+            if (targetObject && targetObject == this.gameObject.GetComponent<Collider2D>())
+            {
+                Debug.Log("picked up sprite");
+                gc.draggingObj = this.gameObject;
+            }
+        }
+    }
+}
