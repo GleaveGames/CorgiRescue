67e01bc3607c7ad851a75a015a774cb22b4570d1
ilovecodinglol <62714727+ilovecodinglol@users.noreply.github.com>
2021-12-14 13:44:04 +0000

    Added Wizard

---
+++ MobileGame/Assets/Wizard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : Unit
+{
+    public override IEnumerator OnBuy()
+    {
+        actioning = true;
+        List<GameObject> allies = GetAllies();
+        allies.Remove(gameObject);
+        if(allies.Count == 0) yield return StartCoroutine(base.OnBuy());
+        else
+        {
+            int randomUnitIndex = Random.Range(0, allies.Count - 1);
+            float buffTimer = 0;
+            GameObject newBuff = Instantiate(Buff, transform.position, Quaternion.identity);
+            while (buffTimer <= buffTime)
+            {
+                newBuff.transform.position = new Vector2(Mathf.Lerp(transform.position.x, allies[randomUnitIndex].transform.position.x, buffX.Evaluate(buffTimer / buffTime)),
+                    Mathf.Lerp(transform.position.y, allies[randomUnitIndex].transform.position.y, buffTimer / buffTime) + 2 * buffY.Evaluate(buffTimer / buffTime));
+                buffTimer += Time.deltaTime;
+                yield return null;
+            }
+
+            Destroy(newBuff);
+            allies[randomUnitIndex].GetComponent<Unit>().attack += attackBuff * level;
+            allies[randomUnitIndex].GetComponent<Unit>().health += healthBuff * level;
+
+            yield return StartCoroutine(base.OnBuy());
+        }
+    }
+}
