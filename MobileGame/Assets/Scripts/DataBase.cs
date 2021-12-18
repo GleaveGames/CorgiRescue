9a62ea19ee4920cea1a114de4cb1e30ba865b678
ilovecodinglol <62714727+ilovecodinglol@users.noreply.github.com>
2021-12-18 21:17:45 +0000

    pre-rework!

--- MobileGame/Assets/Scripts/DataBase.cs
+++ MobileGame/Assets/Scripts/DataBase.cs
@@ -6,21 +6,48 @@ using UnityEngine.Networking;
  
 public class DataBase : MonoBehaviour
 {
-    string url = "https://raw.githubusercontent.com/GleaveGames/CorgiRescue/main/MobileGame/Assets/testdatabase.txt";
+    //string url = "https://raw.githubusercontent.com/GleaveGames/CorgiRescue/main/MobileGame/Assets/testdatabase.txt";
+    string url = "https://localhost:44369/DataBase";
+    string geturl = "https://feudalwars.000webhostapp.com/phpTest.php";
 
 
     public void GetDataBase()
     {
         StartCoroutine(GetText());
+    } 
+    
+    /*public void PostDataBase(int round, string formation)
+    {
+        StartCoroutine(PostText());
     }
+    */
+
 
     IEnumerator GetText()
     {
-        WWW w = new WWW(url);
-        yield return w;
-        Debug.Log(w.text);
-
-        GetComponent<GameController>().databasetext = w.text;
+        UnityWebRequest w = UnityWebRequest.Get(geturl);
+        yield return w.SendWebRequest();
+        if (w.isNetworkError || w.isHttpError) Debug.Log("errer " + w.error);
+        else
+        {
+            Debug.Log(w.downloadHandler.text.Length);
+            GetComponent<GameController>().databasetext = w.downloadHandler.text;
+        }
     }
+
+
+    IEnumerator FindOpponent(string formation)
+    {
+        UnityWebRequest w = UnityWebRequest.Get(geturl);
+        yield return w.SendWebRequest();
+        if (w.isNetworkError || w.isHttpError) Debug.Log("errer " + w.error);
+        else
+        {
+            Debug.Log(w.downloadHandler.text.Length);
+            GetComponent<GameController>().databasetext = w.downloadHandler.text;
+        }
+    }
+
+    
 }
 
