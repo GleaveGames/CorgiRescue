6f5161f6d6a187dbefe9a99730096e3ae8d8cedd
ilovecodinglol <62714727+ilovecodinglol@users.noreply.github.com>
2021-12-31 13:27:22 +0000

    Spearman

---
+++ MobileGame/Assets/Scripts/Spearman.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spearman : Unit
+{
+    public override IEnumerator OnSell()
+    {
+        FindObjectOfType<GameController>().Gold += level;
+        return base.OnSell();
+    }
+}
