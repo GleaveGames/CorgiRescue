b77b00b556ea77f6fddf6e45f055a0b3eb0c402b
ilovecodinglol <62714727+ilovecodinglol@users.noreply.github.com>
2022-01-05 21:52:06 +0000

    Changed the inventor

--- MobileGame/Assets/Scripts/Inventor.cs
+++ MobileGame/Assets/Scripts/Inventor.cs
@@ -6,6 +6,36 @@ public class Inventor : Unit
 {
     public override IEnumerator OnStartOfTurn()
     {
+        actioning = true;
+        Collider2D col = Physics2D.OverlapPoint(transform.position + new Vector3(1.25f, 0, 0));
+        GameSquare square = null;
+        if (col != null) square = col.GetComponent<GameSquare>();
+        if (square != null && square.occupied && square.occupied)
+        {
+            StartCoroutine(Jiggle());
+            float buffTimer = 0;
+            GameObject newBuff = Instantiate(Buff, transform.position, Quaternion.identity);
+            GameObject unit = square.occupier;
+            while (buffTimer <= buffTime)
+            {
+                newBuff.transform.position = new Vector2(Mathf.Lerp(transform.position.x, unit.transform.position.x, buffX.Evaluate(buffTimer / buffTime)),
+                    Mathf.Lerp(transform.position.y, unit.transform.position.y, buffTimer / buffTime) + 2 * buffY.Evaluate(buffTimer / buffTime));
+                buffTimer += Time.deltaTime;
+                yield return null;
+            }
+            Destroy(newBuff);
+            unit.GetComponent<Unit>().health += healthBuff * level;
+            unit.GetComponent<Unit>().attack += attackBuff * level;
+            StartCoroutine(unit.GetComponent<Unit>().BuffJuice(3));
+            StartCoroutine(unit.GetComponent<Unit>().Jiggle());
+        }
+
+        yield return StartCoroutine(base.OnStartOfTurn());
+    }
+
+    /*
+    public override IEnumerator OnStartOfTurn()
+    {
         List<GameObject> allies = GetAllies();
         foreach(GameObject ally in allies)
         {
@@ -39,4 +69,6 @@ public class Inventor : Unit
         StartCoroutine(unit.GetComponent<Unit>().BuffJuice(3));
         StartCoroutine(unit.GetComponent<Unit>().Jiggle());
     }
+
+    */
 }
